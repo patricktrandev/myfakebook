676a46ff85f49a74edaf14feb3b9b934e8918635
patricktrandev <patricktrandev@gmail.com>
2023-04-27 23:17:55 +0800

    add protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,19 @@ import { Login } from "./pages/auth/Login";
 import { Routes, Route } from "react-router-dom";
 import { UserProfile } from "./pages/profile/UserProfile";
 import { Home } from "./pages/Home";
+import ProtectedRoute from "./helpers/ProtectedRoute";
+import NotProtectedRoute from "./helpers/NotProtectedRoute";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} exact />
-      <Route path="/login" element={<Login />} exact />
-      <Route path="/profile" element={<UserProfile />} exact />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/profile" element={<UserProfile />} exact />
+      </Route>
+
+      <Route element={<NotProtectedRoute />}>
+        <Route path="/login" element={<Login />} exact />
+      </Route>
     </Routes>
   );
 }
