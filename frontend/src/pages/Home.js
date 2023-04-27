6a785ee83eddde3f4f1d6dad1ea5a52efdb4c58e
patricktrandev <patricktrandev@gmail.com>
2023-04-28 00:59:39 +0800

    add left menu

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React, { useRef, useState } from "react";
 import { Header } from "../components/headers/Header";
 import useClickOutside from "../helpers/outsideClick";
+import { LeftSideMenu } from "../components/home/leftSide/LeftSideMenu";
+import { useSelector } from "react-redux";
 
 export const Home = () => {
+  const { user } = useSelector((user) => ({ ...user }));
   return (
     <div>
       <Header />
+      <LeftSideMenu user={user} />
     </div>
   );
 };
